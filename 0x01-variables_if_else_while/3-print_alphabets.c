b1ae7634f490b28156226cea3e2f97d42f397762
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-10-06 03:13:02 -0700

    Add C file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowerLetter = 'a';
+	char upperLetter = 'A';
+	while (lowLetter <= 'z')
+	{
+		putchar(lowLetter);
+		lowLetter++;
+	}
+
+	while (upperLetter <= 'Z')
+	{
+		putchar(upperLetter);
+		upperLetter++;
+	}
+
+	putchar('\n');
+	return (0);
+}
