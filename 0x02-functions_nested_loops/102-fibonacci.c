83c48b02ecc175fe26640d2bea493570ec654f86
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-10-10 10:41:58 -0700

    Add C file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 3;
+	long int n1 = 1, n2 = 2, sum = n1 + n2;
+
+	while (i < 51)
+	{
+		if (i == 50)
+			printf("%lu\n", sum);
+		else
+			printf("%lu, ", sum);
+		n1 = n2;
+		n2 = sum;
+		i++;
+	}
+
+	return (0);
+}
