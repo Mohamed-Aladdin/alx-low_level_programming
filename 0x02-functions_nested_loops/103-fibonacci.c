73adbcc47383c7d690d64a4a095c919684c10164
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-10-10 10:58:46 -0700

    Add C file

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int n1 = 1, n2 = 2, sum = n1 + n2;
+	long int total = 0;
+
+	printf("%lu, ", n1);
+	printf("%lu, ", n2);
+
+	while (sum < 4000000)
+	{
+		if (sum % 2 == 0)
+			total += sum;
+		n1 = n2;
+		n2 = sum;
+		sum = n1 + n2;
+	}
+
+	printf ("%lu\n", total);
+	return (0);
+}
