92fc5a96e0ec91f0b3ac92e6d62beedae48e87cf
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-10-10 10:27:38 -0700

    Add C file

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,13 +5,13 @@
  */
 int main(void)
 {
-	int i = 0;
-	int sum = 0;
+	int i = 0, int sum = 0;
 
 	while (i < 1024)
 	{
 		if ((i % 3) == 0 || (i % 5) == 0)
 			sum += i;
+		i++;
 	}
 
 	printf("%d\n", sum);
