09474af0ef510b13455f693e74257ae87811b315
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-10-10 05:30:07 -0700

    Fix C file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,16 @@ void print_alphabet_x10(void)
 {
 	int i = 0;
 
-	while (i <= 10)
+	while (i < 10)
 	{
 		char letter = 'a';
 
-        	while (letter <= 'z')
-        	{
-                	_putchar(letter);
-                	letter++;
-        	}
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
 
-        	_putchar('\n');
+		_putchar('\n');
 	}
 }
