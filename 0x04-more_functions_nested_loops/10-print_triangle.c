b41375240746ddbfee31fcee719220449854e768
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-10-12 05:31:30 -0700

    Add C files

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * print_triangle - prints a triangle, followed by a new line. 
+ * print_triangle - prints a triangle, followed by a new line.
  * @size: The size of the triangle.
- * 
  * Return: void.
  */
 
