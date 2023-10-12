72d6583a891490dc655cf755288e0421b9d8a0df
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-10-12 03:44:00 -0700

    Add C files

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * print_most_numbers -  prints the numbers, from 0 to 9, followed by a new line.
+ * print_most_numbers - prints the numbers,
+ * from 0 to 9, followed by a new line.
  * Return: void.
  */
 
@@ -11,11 +12,12 @@ void print_most_numbers(void)
 	while (i < 10)
 	{
 		if (i == 2 || i == 4)
+		{
 			i++;
 			continue;
+		}
 		_putchar('0' + i);
 		i++;
-	}
-	
+	}	
 	_putchar('\n');
 }
