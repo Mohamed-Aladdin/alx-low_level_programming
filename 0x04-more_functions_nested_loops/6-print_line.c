fb8041cd34a00c733798465acd6ae06ea25af2f7
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-10-12 04:25:31 -0700

    Add C files

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * print_line - draws a straight line in the terminal.
+ * print_line - draws a straight line
+ * in the terminal.
  * @n: the number of times the
  * character _ should be printed.
  * 
