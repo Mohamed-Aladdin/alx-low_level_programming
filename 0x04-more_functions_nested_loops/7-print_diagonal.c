83db961ba031ec1a32d61f53ceb133cd536e21d1
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-10-12 03:14:42 -0700

    Add C file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonal line on the terminal.
+ * @n: the number of times the character \ should be printed.
+ * 
+ * Return: void.
+ */
+
+void print_diagonal(int n)
+{
+	int i = 1, j = 0;
+
+	while (i <= n)
+	{
+		while (j < i)
+		{
+			_putchar(' ');
+			j++;
+		}
+
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+
+	if (n <= 0)
+		_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - prints a square, followed by a new line. 
+ * @size: The size of the square.
+ * 
+ * Return: void.
+ */
+
+void print_square(int size)
+{
+	int i = 1, j = 0;
+
+	while (i <= n)
+	{
+		while (j < size)
+		{
+			_putchar('#');
+			j++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+
+	if (n <= 0)
+		_putchar('\n');
+}
