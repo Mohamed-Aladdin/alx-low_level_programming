7575e68581b53fba16de39a392390cf5a7430c24
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-10-12 04:32:50 -0700

    Add C files

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * print_diagonal - draws a diagonal line on the terminal.
- * @n: the number of times the character \ should be printed.
- * 
+ * print_diagonal - draws a diagonal
+ * line on the terminal.
+ * @n: the number of times the
+ * character \ should be printed.
  * Return: void.
  */
 
