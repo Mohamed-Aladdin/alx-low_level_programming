b472cec3dd27be7c4ff911324ac393c7b06370a7
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-10-16 03:14:19 -0700

    Add C files

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @s: The given string.
+ *
+ * Return: Void.
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
