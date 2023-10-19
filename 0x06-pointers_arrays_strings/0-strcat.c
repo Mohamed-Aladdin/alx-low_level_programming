67b5616963ee09f040a0c311cd3ed1d3b56dff46
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-10-19 01:48:05 -0700

    Fix C file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * _strcat - appends the src string to the dest string,
- * overwriting the terminating null byte (\0) at the end of dest,
+ * _strcat - appends the src string to the dest
+ * string,overwriting the terminating null byte
+ * (\0) at the end of dest,
  * and then adds a terminating null byte.
  * @dest: The first pointer.
  * @src: The second pointer.
