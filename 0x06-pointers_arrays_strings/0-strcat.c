35b00e7bcbe8536e16f85e03363f49bbc668a9a3
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-10-19 02:01:04 -0700

    Fix C file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -21,11 +21,12 @@ char *_strcat(char *dest, char *src)
 		i++;
 	}
 
-	for (j = 0; src[j] != '\0'; j++)
+	while (src[j] != '\0')
 	{
 		dest[i+j] = src[j];
+		j++;
 	}
-	dest[i+j] = '\0';
 
+	dest[i+j] = '\0';
 	return (dest);
 }
