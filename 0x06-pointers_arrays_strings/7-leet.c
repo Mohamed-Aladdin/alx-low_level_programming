cc6c55b57a1a0473b0086dfa57ca5403b5876027
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-10-19 03:11:07 -0700

    Fix C file

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *leet -  encodes a string into 1337.
+ * leet -  encodes a string into 1337.
  * @*s: The given string.
  *
  * Return: a pointer to the resulting string s.
