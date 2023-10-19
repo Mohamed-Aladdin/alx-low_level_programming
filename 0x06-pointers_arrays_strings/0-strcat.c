1bd0ccce391c474babc1b0972850332d4c209a82
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-10-19 02:03:14 -0700

    Fix C file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,13 +8,13 @@
  * @dest: The first pointer.
  * @src: The second pointer.
  *
- * Return: a pointer to the resulting string dest.
+ * Return: a pointer to the resulting
+ * string dest.
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int j = 0;
+	int i = 0, j = 0;
 
 	while (dest[i] != '\0')
 	{
