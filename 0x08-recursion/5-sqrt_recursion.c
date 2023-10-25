b02d2327e7b1b4a3c8aad7207bd028ea7650a167
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-10-25 02:57:30 -0700

    Add C file

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * root_checker - checks for square root.
+ * @num: The number.
+ * @sqrt_root: The root.
+ *
+ * Return: an Integer.
+ */
+
+int root_checker(int num, int sqrt_root)
+{
+	if (sqrt_root * sqrt_root == num)
+		return (sqrt_root);
+	else if(sqrt_root * sqrt_root > num)
+		return (-1);
+
+	return (root-checker(num, sqrt_root + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: The number.
+ *
+ * Return: The natural square root of a number.
+ */
+
+int _sqrt_recursion(int n)
+{
+	int sqrt_root = n;
+
+	if (n < 0)
+		return (-1);
+	else if (y == 1)
+		return (1);
+
+	return (root_checker(n, 1));
+}
