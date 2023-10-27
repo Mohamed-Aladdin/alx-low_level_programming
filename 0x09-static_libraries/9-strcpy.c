6f273e4725654d00f1326afdf4c901f4a1fd283f
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-10-27 05:15:46 -0700

    Fix lib file

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "2-strlen.c"
 
 /**
  * _strcpy - copies the string pointed to by src, including the
@@ -9,4 +10,15 @@
  * Return: The pointer to dest.
  */
 
-char *_strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0, length = _strlen(src);
+
+	while (i <= length)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
