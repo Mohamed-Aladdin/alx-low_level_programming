4175cb3e239d1b6df14f839dc5653c70b62e269e
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-10-27 06:23:23 -0700

    Add C file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - Entry point.
+ * @argc: The number of arguments.
+ * @argv: The array of arguments.
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int i, mul = 1;
+
+	for (i = 0; i < argc; i++)
+	{
+		mul *= i;
+		printf("%d\n", i);
+	}
+	return (0);
+}
