86731fdf2e35cdc29b8af7e3ed92416c1fb70f6b
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-10-27 06:10:35 -0700

    Add C file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point.
+ * @argc: The number of arguments.
+ * @argv: The array of arguments.
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%d\n", argc);
+	return (0);
+}
