28709a2145a80ee7090905514179e4f4cc531e45
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-10-30 07:40:00 -0700

    Add C file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,7 +10,7 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, length;
+	int i, j = 0, length;
 	char *full_str;
 
 	if (s1 == NULL)
@@ -18,7 +18,7 @@ char *str_concat(char *s1, char *s2)
 	else if (s2 == NULL)
 		s2 = "";
 
-	length = _strlen(s1) + _strlen(s2) + 2;
+	length = _strlen(s1) + _strlen(s2) + 1;
 
 	full_str = malloc(length * sizeof(char));
 
