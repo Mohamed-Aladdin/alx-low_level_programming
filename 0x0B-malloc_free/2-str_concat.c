9d8e10c706a38f85cf0d0a25b8fdc8e394c3e8df
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-10-30 07:34:43 -0700

    Add C file

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include "_strlen"
+/**
+ * *str_concat - concatenates two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: Char.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, length;
+	char *full_str;
+
+	if (s1 == NULL)
+		s1 = "";
+	else if (s2 == NULL)
+		s2 = "";
+
+	length = _strlen(s1) + _strlen(s2);
+
+	full_str = malloc(length * sizeof(char));
+
+	if (full_str == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i]; i++)
+	{
+		full_str[i++] = s1[i];
+	}
+
+	for (i = 0; s2[i]; i++)
+	{
+		full_str[i++] = s2[i];
+	}
+
+	return (full_str);
+
+
+}
