d79ea1fe648160a2e83b07caf236a9ca14882e30
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-11-02 04:53:48 -0700

    Add C file

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,8 +14,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (old_size == new_size)
 		return (ptr);
 
-	if (ptr ==NULL)
-		ptr = malloc(new_size);
+	/*if (ptr == NULL)
+		ptr = malloc(new_size);*/
 
 	if (ptr != NULL && new_size == 0)
 	{
