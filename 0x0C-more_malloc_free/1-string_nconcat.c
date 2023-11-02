3f6e810bd5a4616977f61ba86f3e434812cdf2c4
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-11-02 04:08:28 -0700

    Add C file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		full_str[j++] = s1[i];
 	}
 
-	for (i = 0; i < l && s[i]; i++)
+	for (i = 0; i < l && s2[i]; i++)
 	{
 		full_str[j++] = s2[i];
 	}
