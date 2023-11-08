e269f6285cf9c044151be8d48ff0dfcffe5523a6
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-11-08 14:18:05 +0000

    Add C file

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point.
+ * @argc: The number of arguments.
+ * @argv: The array of arguments.
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	unsigned char *ptr = (char *) main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (atoi(argv[1] < 0))
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < (atoi(argv[1]) - 1); ptr++)
+		printf("%02x ", *ptr);
+
+	printf("%02x\n", *ptr);
+
+	return (0);
+}
