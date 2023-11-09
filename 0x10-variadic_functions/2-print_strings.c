44a0c78a5251ea845bd863f0b6786129b6fc0483
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-11-09 11:36:48 +0000

    Add C files

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,13 +12,16 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list list;
 	unsigned int i;
+	char *str
 
 	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
-		if (va_arg(list, char *) != NULL)
-			printf("%s", va_arg(list, char *));
+		str = va_arg(list, char *);
+
+		if (str != NULL)
+			printf("%s", str);
 		else
 			printf("(nil)");
 
