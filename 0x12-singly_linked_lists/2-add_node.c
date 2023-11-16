9460626ad07868bc571ce71dfceb9fc2dc3b74bf
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-11-16 12:45:39 +0000

    Fix C file

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,6 +16,9 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	new_node = malloc(sizeof(list_t));
 
+	if (new_node == NULL)
+		return (NULL);
+
 	new_node->str = strdup(str);
 
 	new_node->len = strlen(str);
