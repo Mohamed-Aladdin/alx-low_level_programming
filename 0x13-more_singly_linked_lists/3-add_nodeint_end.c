c453227e286fa8b5be9b3f537725dc44b966d611
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-11-20 17:46:55 +0000

    Fix C files

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,7 +13,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *last;
 	listint_t *new_node;
 
-	if (head == NULL || n == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
