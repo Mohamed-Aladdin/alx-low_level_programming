0c89cb75926359120baf3bedae1aff771222f98e
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-11-20 17:57:01 +0000

    Fix C files

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *current, *temp;
 	unsigned int i;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (-1);
 	current = *head;
 
