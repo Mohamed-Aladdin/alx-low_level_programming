ab1e41afaca41dea0da5437e0e6a972048ff9b99
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-03-20 17:35:34 +0000

    Add technical interview task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,17 +5,17 @@
 def island_perimeter(grid):
     """Function to calculate the perimeter of the island grid"""
 
-    perim = 0
+    answer = 0
 
-    for j, row in enumerate(grid):
-        for i, cell in enumerate(row):
+    for y, row in enumerate(grid):
+        for x, cell in enumerate(row):
             if cell == 1:
                 if y == 0 or grid[y - 1][x] == 0:
-                    perim += 1
+                    answer += 1
                 if y == len(grid) - 1 or grid[y + 1][x] == 0:
-                    perim += 1
+                    answer += 1
                 if x == 0 or grid[y][x - 1] == 0:
-                    perim += 1
+                    answer += 1
                 if x == len(row) - 1 or grid[y][x + 1] == 0:
-                    perim += 1
-    return perim
+                    answer += 1
+    return answer
