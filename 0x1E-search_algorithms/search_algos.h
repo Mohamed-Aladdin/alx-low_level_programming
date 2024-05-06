6e61ee4c87e2a8e81f8ec29b946ae8ab36176f53
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-05-06 09:02:22 +0000

    Add advanced tasks

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -50,4 +50,9 @@ int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
 
+/* Helper Functions */
+void print_helper(int *array, int start, int end);
+int binary_helper(int *array, int value, size_t low, size_t high);
+int adv_bhelper(int *array, int value, size_t low, size_t high);
+
 #endif /* SEARCH_ALGOS_H */
